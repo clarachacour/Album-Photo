14bb82c52997cef2b3ce36213b7efcb79397e02f
clarachacour <clarachacourc@gmail.com>
2026-08-29 14:44:38 +0200

    Fix import statement formatting in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react"; 
-import "@/App.css";
+import "@/App.css"; 
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "sonner";
 
